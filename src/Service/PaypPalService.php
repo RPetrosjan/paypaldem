7618601fa571af6e4fcd8064debf171a759bf608
ruben <ruben@petrosyan.website>
2023-10-15 03:32:15 +0200

    Pacjegist

---
+++ src/Service/PaypPalService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Paypaldem;
+
+/**
+ * Recuperation API TOKEN PAYPAL ET CONFIRMATION PAYEMENT
+ */
+class PaypPalService
+{
+    public function getInfoPayPalService()
+    {
+        return 'Salut Ruben';
+    }
+}
